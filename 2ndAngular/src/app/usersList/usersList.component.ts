68a26ff400db72dfe8630d7e70289c796dd18f49
Mistyfeatherz <109124631+NatalieParker@users.noreply.github.com>
2024-12-23 09:16:17 -0800

    Creation of variables
    Created and displayed variables using UserListComponent ts and html.

--- 2ndAngular/src/app/usersList/usersList.component.ts
+++ 2ndAngular/src/app/usersList/usersList.component.ts
@@ -5,4 +5,28 @@ import { Component } from "@angular/core";
     templateUrl: './usersList.component.html',
     standalone: false
 })
-export class UsersListComponent {}
+export class UsersListComponent {
+    // testUsers = ["Dave", "John", "Jake", "Dirk"];
+    users = [
+        {
+            id: '1',
+            name: "John",
+            age: 13
+        },
+        {
+            id: '2',
+            name: "Rose",
+            age: 15
+        },
+        {
+            id: '3',
+            name: "Dave",
+            age: 17
+        },
+        {
+            id: '4',
+            name: "Jade",
+            age: 19
+        }
+    ]
+}
