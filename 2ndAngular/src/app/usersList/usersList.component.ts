90a716c69de5eb03c1f1a2b18e507f88181fcd08
Mistyfeatherz <109124631+NatalieParker@users.noreply.github.com>
2024-12-23 09:02:13 -0800

    Created UsersListComponent and Module

---
+++ 2ndAngular/src/app/usersList/usersList.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-users-list',
+    templateUrl: './usersList.component.html',
+    standalone: false
+})
+export class UsersListComponent {}
