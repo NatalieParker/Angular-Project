e1801189e774d95d051de3df0a1bee8f610e5563
Mistyfeatherz <109124631+NatalieParker@users.noreply.github.com>
2024-12-23 10:39:58 -0800

    created userList.component.scss
    Started editing display of website

--- 2ndAngular/src/app/usersList/usersList.component.ts
+++ 2ndAngular/src/app/usersList/usersList.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-users-list',
     templateUrl: './usersList.component.html',
+    styleUrl: './usersList.component.scss',
     standalone: false
 })
 export class UsersListComponent {
