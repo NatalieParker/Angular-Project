e8a6afc02ce9438a6e86c208a9d051469f27297e
Mistyfeatherz <109124631+NatalieParker@users.noreply.github.com>
2024-12-24 15:57:14 -0800

    Began AngularProject
    Started creating practice Angular project. Created a page /register and handled routing, and also organized folders correctly.

---
+++ AngularProject/src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RegisterComponent } from "./components/register/register.component";
+import { RouterModule } from "@angular/router";
+
+const routes = [
+    {
+        path: "register",
+        component: RegisterComponent
+    }
+];
+
+@NgModule({
+    imports: [CommonModule, RouterModule.forChild(routes)],
+    declarations: [RegisterComponent]
+})
+
+export class AuthModule {}
